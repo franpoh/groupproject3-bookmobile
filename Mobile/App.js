cdeed2bb16246df1c6564a4e1dfecd08a369f0b8
AuntPyoneMaung <za7196@gmail.com>
2022-02-22 11:56:39 +0800

    imported App.js from franpoh

--- Mobile/App.js
+++ Mobile/App.js
@@ -7,6 +7,7 @@ import { createMaterialBottomTabNavigator } from '@react-navigation/material-bot
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 import colours from './style_constants/colours';
+import AuthContext from './context';
 
 import AccountScreen from './screens/account';
 import WishlistScreen from './screens/wishlist';
@@ -16,37 +17,27 @@ import BookDetailsScreen from './screens/book_details';
 import BookListScreen from './screens/book_list';
 import UploadBookScreen from './screens/upload_book';
 import UploadReviewScreen from './screens/upload_review';
-
-const Stack = createNativeStackNavigator();
+import SplashScreen from './components/splash';
 
 // Issue: When native stacks are nested inside material bottom tabs navigator (react-navigation), navigating between tabs back and forth causes a blank screen to appear.
 // Workaround: https://github.com/software-mansion/react-native-screens/issues/1197#issuecomment-993682256
 // Will probably be fixed in the future
 
-const AccountStack = () => {
-    return (
-        <View style={{ flex: 1 }} collapsable={false}>
-            <Stack.Navigator
-                initialRouteName='AccountPage'
-            >
-                <Stack.Screen name="AccountPage" component={AccountScreen} />
-                <Stack.Screen name="Login" component={LoginScreen} />
-                <Stack.Screen name="Sign Up" component={SignUpScreen} />
-            </Stack.Navigator>
-        </View>
-    )
-}
+const BookStack = createNativeStackNavigator();
 
-const BookStack = () => {
+const BookStackScreen = () => {
     return (
         <View style={{ flex: 1 }} collapsable={false}>
-            <Stack.Navigator
-                initialRouteName='Books'
+            <BookStack.Navigator
+                initialRouteName='Upload Book'
+                screenOptions={{
+                    headerShown: false
+                }}
             >
-                <Stack.Screen name="Book List" component={BookListScreen} />
-                <Stack.Screen name="Book Details" component={BookDetailsScreen} />
-                <Stack.Screen name="Upload Review" component={UploadReviewScreen} />
-            </Stack.Navigator>
+                <BookStack.Screen name='Upload Book' component={UploadBookScreen} />
+                <BookStack.Screen name="Book Details" component={BookDetailsScreen} />
+                <BookStack.Screen name="Review Upload" component={UploadReviewScreen} />
+            </BookStack.Navigator>
         </View>
     )
 }
@@ -55,108 +46,161 @@ const BookStack = () => {
 // For integration with React Navigation, you can use react-navigation-material-bottom-tabs and consult createMaterialBottomTabNavigator documentation
 // https://reactnavigation.org/docs/material-bottom-tab-navigator/
 
-const Tab = createMaterialBottomTabNavigator();
-
 // Handling nesting bottom navigation
 // https://reactnavigation.org/docs/nesting-navigators/
 
-const TabNavigator = () => {
+const RootStack = createMaterialBottomTabNavigator();
+
+const RootStackScreen = ({ userToken }) => {
+    console.log("In Stack", userToken);
+
     return (
-        <Tab.Navigator
+        <RootStack.Navigator
             initialRouteName='Books'
             activeColor={colours.primary}
             labelStyle={{ fontSize: 12 }}
             barStyle={{ backgroundColor: colours.secondaryLight }}
         >
-            <Tab.Screen
-                name='Books'
-                component={BookStack}
-                options={{
-                    tabBarLabel: 'Books',
-                    tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons
-                            name='bookshelf'
-                            color={color}
-                            size={26}
-                        />
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name='Wishlist'
-                component={WishlistScreen}
-                options={{
-                    tabBarLabel: 'Wishlist',
-                    tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons
-                            name='heart'
-                            color={color}
-                            size={26}
-                        />
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name='UploadBook'
-                component={UploadBookScreen}
-                options={{
-                    tabBarLabel: 'Upload Book',
-                    tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons
-                            name='book-plus'
-                            color={color}
-                            size={26}
-                        />
-                    ),
-                }}
-            />
-            <Tab.Screen
-                name='Account'
-                component={AccountStack}
-                options={{
-                    tabBarLabel: 'Account',
-                    tabBarIcon: ({ color }) => (
-                        <MaterialCommunityIcons
-                            name='account'
-                            color={color}
-                            size={26}
-                        />
-                    ),
-                }}
-            />
-        </Tab.Navigator>
+            <RootStack.Group screenOptions={{ presentation: 'modal' }}>
+                <RootStack.Screen
+                    name='Book List'
+                    component={BookListScreen}
+                    options={{
+                        tabBarLabel: 'Book List',
+                        tabBarIcon: ({ color }) => (
+                            <MaterialCommunityIcons
+                                name='bookshelf'
+                                color={color}
+                                size={26}
+                            />
+                        ),
+                    }}
+                />
+            </RootStack.Group>
+            {userToken ? (
+                <RootStack.Group>
+                    <RootStack.Screen
+                        name='Wishlist'
+                        component={WishlistScreen}
+                        options={{
+                            tabBarLabel: 'Wishlist',
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons
+                                    name='heart'
+                                    color={color}
+                                    size={26}
+                                />
+                            ),
+                        }}
+                    />
+
+                    <RootStack.Screen
+                        name='Book Upload'
+                        component={BookStackScreen}
+                        options={{
+                            tabBarLabel: 'Book Upload',
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons
+                                    name='book-plus'
+                                    color={color}
+                                    size={26}
+                                />
+                            ),
+                        }}
+                    />
+
+                    <RootStack.Screen
+                        name='Account'
+                        component={AccountScreen}
+                        options={{
+                            tabBarLabel: 'Account',
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons
+                                    name='account'
+                                    color={color}
+                                    size={26}
+                                />
+                            ),
+                        }}
+                    />
+                </RootStack.Group>
+            ) : (
+                <RootStack.Group>
+                    <RootStack.Screen
+                        name='Login'
+                        component={LoginScreen}
+                        options={{
+                            tabBarLabel: 'Login',
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons
+                                    name='account'
+                                    color={color}
+                                    size={26}
+                                />
+                            ),
+                        }}
+                    />
+
+                    <RootStack.Screen
+                        name='Sign Up'
+                        component={SignUpScreen}
+                        options={{
+                            tabBarLabel: 'Sign Up',
+                            tabBarIcon: ({ color }) => (
+                                <MaterialCommunityIcons
+                                    name='account-plus'
+                                    color={color}
+                                    size={26}
+                                />
+                            ),
+                        }}
+                    />
+                </RootStack.Group>
+            )}
+        </RootStack.Navigator>
     )
 }
 
-export default function App() {
-    return (
-        <NavigationContainer>
-            <TabNavigator />
-        </NavigationContainer>
-    )
-}
-
-// Future Implementation
 // Handling permission in routes with Group
 // https://reactnavigation.org/docs/group/
 
-// <Stack.Navigator>
-//   {isLoggedIn ? (
-//     // Screens for logged in users
-//     <Stack.Group>
-//       <Stack.Screen name="Home" component={Home} />
-//       <Stack.Screen name="Profile" component={Profile} />
-//     </Stack.Group>
-//   ) : (
-//     // Auth screens
-//     <Stack.Group screenOptions={{ headerShown: false }}>
-//       <Stack.Screen name="SignIn" component={SignIn} />
-//       <Stack.Screen name="SignUp" component={SignUp} />
-//     </Stack.Group>
-//   )}
-//   {/* Common modal screens */}
-//   <Stack.Group screenOptions={{ presentation: 'modal' }}>
-//     <Stack.Screen name="Help" component={Help} />
-//     <Stack.Screen name="Invite" component={Invite} />
-//   </Stack.Group>
-// </Stack.Navigator>
+export default function App() {
+    const [isLoading, setIsLoading] = React.useState(true);
+    const [userToken, setUserToken] = React.useState(null);
+
+    const authContext = React.useMemo(() => { // useMemo cache a value so that it does not need to be recalculated
+        return {
+            signIn: () => {
+                setIsLoading(false);
+                setUserToken("mellon");
+            },
+            signUp: () => {
+                setIsLoading(false);
+                setUserToken("mellon");
+            },
+            signOut: () => {
+                setIsLoading(false);
+                setUserToken(null);
+            },
+            user: userToken,
+        };
+    }, []);
+
+    React.useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 1000);
+    }, [])
+
+    if (isLoading) {
+        return <SplashScreen />
+    }
+
+    return (
+        <AuthContext.Provider value={authContext} >
+            <NavigationContainer>
+                <RootStackScreen userToken={userToken} />
+            </NavigationContainer>
+        </AuthContext.Provider>
+    )
+}
