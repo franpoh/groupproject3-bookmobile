83ed9500a5cb67769e40b880f78923c21e8d62ed
AuntPyoneMaung <za7196@gmail.com>
2022-02-22 23:25:23 +0800

    books retrievable from mock data and search filter but unable to display

--- Mobile/screens/book_list/components/book_search.js
+++ Mobile/screens/book_list/components/book_search.js
@@ -1,21 +1,47 @@
-import * as React from 'react';
-import { Caption, Searchbar, Provider as PaperP } from "react-native-paper";
+import React, { useState, useEffect } from 'react';
+import { ScrollView, StyleSheet, Image, View, TouchableOpacity, TextInput } from 'react-native';
+import { Text, Searchbar, Provider as PaperP } from "react-native-paper";
+import { userA, indexBooks, swap } from '../../../components/test-data';
+import GetBooks from '../getBooks';
+
 
 
 const BookSearch = () => {
-    const [searchQuery, setSearchQuery] = React.useState('');
+    // const [searchQuery, setSearchQuery] = React.useState('');
     const onChangeSearch = query => setSearchQuery(query);
+    const [search, setSearch] = React.useState('');
+    const [filteredData, setFilteredData] = React.useState([]);
+    const localIndexBooks = indexBooks;
 
+    // backend
+    // useEffect(() => {
+    //   fetch("https://localhost).then((response) => {}})
+    // },  []);
+
+    const searchFilter = (text) => {
+        if (text) {
+            const newData = localIndexBooks.filter(item => {
+                const itemData = item.title ? item.title.toUpperCase() : ''.toUpperCase();
+                const textData = text.toUpperCase();
+                return itemData.indexOf(textData) > -1;
+            })
+            setFilteredData(newData);
+            console.log(newData);
+            setSearch(text);
+        } else {
+            setFilteredData(localIndexBooks);
+            setSearch(text);
+        }
+
+    }
 
     return (
         <PaperP>
             <Searchbar
                 placeholder="Search"
-                onChangeText={onChangeSearch}
-                // onChangeText={(event) => {
-                //     searchFilter(event.nativeEvent.text);
-                // }} // use with useEffect(), [xxx]
-                value={searchQuery}
+                // onChangeText={onChangeSearch}
+                onChangeText={(event) => searchFilter(event)}
+                value={search}
                 icon='book-search'
             />
         </PaperP>
@@ -24,19 +50,7 @@ const BookSearch = () => {
     );
 };
 
-// with data from API link 
-// const searchFilter = (text) => {
-//     if (text) {
-//         const newData = data.filter(item => {
-//             const itemData = item.name.first ? item.name.first.toUpperCase() : ''.toUpperCase();
-//             const textData = text.toUpperCase();
-//             return itemData.indexOf(textData) > -1;
-//         })
-//         setFilteredData(newData);
-//     } else {
-//         setFilteredData(data);
-//     }
 
-// }
 
 export default BookSearch;
+
