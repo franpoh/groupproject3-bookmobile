9ef98bf2779990b62507c3cc3713b1e8501d3160
franpoh <91236694+franpoh@users.noreply.github.com>
2022-02-22 11:30:50 +0800

    edited screen navigation

--- Mobile/screens/signup/index.js
+++ Mobile/screens/signup/index.js
@@ -3,28 +3,33 @@ import { Text, View, Alert } from "react-native";
 
 import MyButton from "../../components/button";
 import styles from "../../style_constants/style-sheet";
+import AuthContext from "../../context";
 
-const SignUpScreen = ({ navigation }) => (
-  <View style={styles.container}>
-    <Text>Sign Up</Text>
-    
-    <MyButton 
-      text="Submit" 
-      buttonAction={ 
-        () => {
-          Alert.alert(
-            'Placeholder',
-            "Sign Up Successful",
-            [
-              { text: "OK" }
-            ]
-          );
-          navigation.navigate('Account', { screen: 'AccountPage'});
+const SignUpScreen = ({ navigation }) => {
+  const { signUp } = React.useContext(AuthContext);
+
+  return (
+    <View style={styles.container}>
+      <Text>Sign Up</Text>
+
+      <MyButton
+        text="Submit"
+        buttonAction={
+          () => {
+            Alert.alert(
+              'Placeholder',
+              "Sign Up Successful",
+              [
+                { text: "OK" }
+              ]
+            );
+            signUp();
+          }
         }
-      }
-    />
+      />
 
-  </View>
-);
+    </View>
+  )
+};
 
 export default SignUpScreen;
